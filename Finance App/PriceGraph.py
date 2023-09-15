fb798a888f519c261075e20397ebe8847d4e0e07
Jeffrey Scoggins <JeffreyScoggins84@gmail.com>
2023-09-15 00:36:56 -0400

    pricegraph

--- Finance App/PriceGraph.py
+++ Finance App/PriceGraph.py
@@ -6,6 +6,42 @@ from datetime import datetime
 plt.style.use('seaborn')
 
 
+
+def searchreturn():
+    interval = [ "1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max" ]
+    return interval
+
+
+def oneDay(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('1d')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
+def fiveDay(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('5d')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
+def oneMonth(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('1mo')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
 def threeMonth(symbol):
 
     tk = yf.Ticker(symbol)
@@ -14,4 +50,78 @@ def threeMonth(symbol):
 
     plt.figure
     plt.plot(df['Close'])
-    plt.savefig('stock3month.png')
+    plt.savefig('stockprice.png')
+
+def sixMonth(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('6mo')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
+def ytd(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('ytd')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
+
+def oneYear(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('1y')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
+def twoYear(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('2y')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
+def fiveYear(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('5y')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
+def tenYear(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('10y')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
+def max(symbol):
+
+    tk = yf.Ticker(symbol)
+
+    df = tk.history('max')
+
+    plt.figure
+    plt.plot(df['Close'])
+    plt.savefig('stockprice.png')
+
+
+
