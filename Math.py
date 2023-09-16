063bc8d264e5bb52fa23e43322075f3aed301655
Jeffrey Scoggins <JeffreyScoggins84@gmail.com>
2023-09-16 15:45:05 -0400

    Added Mathmatics Class

---
+++ Math.py
@@ -0,0 +1,19 @@
+'''
+o	Black Sholes Equation for options price calculation
+o	Return on Equity (ROE) = (Net income/shareholder equity)
+o	Price/Earnings Ratio (P/E) = Market price of Stock/Earnings per share
+o	ROA = Operating income / total assets
+o	ROE = Net Income / Equity
+o	EPS = Net Income / WTD AVG C/S Outstanding
+o	GP Margin = GP/ Sales
+o	Operating Profit Margin = Operating income / sales
+o	Net Profit Margin = Net Income / Sales
+o	Operating Cost Ration = Marketing & Admin Exp / Sales
+o	CF to Revenue = Operating CF / Net Revenue
+o	Cash to income = Operating CF / Operating Income from Income Statement
+o	Current Ratio = Current Assets / Current Liabilities
+o	Quick Ratio = (Cash + Marketable Securities + A/R) / Current Liabilities
+o	Debt Ratio = Total Debt / Total Assets
+o	Cash Ration (Cash + Marketable Securities / Current Liabilities
+o	Inventory turnover ratio COGS / inventory
+'''
